0dcbce394452498e311cecb15d62d0f2895a8af5
andela-eakinyele <emmanuel.akinyele@andela.com>
2015-10-20 14:56:36 +0100

    core(db-backend) : config.js and database.js
    - development and staging db config data in config.js
    - created connection instance in database.js
    - Updated server.js for testing connection instance
    
    Closes #26

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+// definition of config object for postgres database
+var config = {
+  development: {
+    host: 'localhost',
+    user: 'ubuntu',
+    password: '',
+    database: 'kipit',
+    port: 5432
+  },
+  // test config object
+  test: {
+    host: 'localhost',
+    user: 'ubuntu',
+    password: '',
+    database: 'circle_test',
+    port: 5432
+  }
+};
+module.exports = config;
