f9ffe9cc0ff9e4591a4875c669e895a22188e1a8
Maryam <maryamolabisi02@gmail.com>
2015-10-20 12:14:45 +0100

    issue #39-test set-up

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
+var path = require('path');
 var express = require('express');
+
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('Welcome to Kipit');
-});
-
-var server = app.listen(3000, function(){
-	var host = server.address().address;
-	var port = server.address.port;
-	
+app.get('/', function(res, req){
+  res.send('')
+})
+app.listen(3000);
 });
