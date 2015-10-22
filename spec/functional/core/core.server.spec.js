c9d605811f674c940e9be7530d653dc9ac669fa0
andela-blawrence <lawrence.olaiya@andela.com>
2015-10-22 13:27:17 +0100

    Modifications to spec files.
    A bit of modification made to the functional and unit tests.
    
    Corrections made in each file respectively.

--- spec/functional/core/core.server.spec.js
+++ spec/functional/core/core.server.spec.js
@@ -1,9 +1,13 @@
 var request = require("supertest"),
-    app = require("server");
-describe("Server : ", function() {
-    describe("Server should return appropraite server response.", function() {
-        it("should response to /", function(done) {
-            request(app).get('/').expect('Content-Type', /json/).expect(200).end(function(err, res) {
+    app = require("./server");
+describe("Server", function() {
+    describe("should return appropraite server response.", function() {
+        it("Should response to /", function(done) {
+            request(app)
+            .get("/")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end(function(err, res) {
                 if (err) throw err;
             });
         });
--- spec/unit/database/database.spec.js
+++ spec/unit/database/database.spec.js
@@ -1,11 +1,20 @@
-var db_config = require('config');
-describe("Database : ", function() {
+var db_config = require("./config");
+describe("Database", function() {
     describe("Application", function() {
-        it("should be connected to the correct database on development.", function() {
-            expect(db_config.development.database).toBe('kipit');
-        });
-        it("should be connected to the correct database on staging", function() {
-            expect(db_config.staging.database).toBe('circle-test');
+    	beforeEach(function(){
+			var node_env = process.env.NODE_ENV;
+		});
+
+        it("should be connected to the correct database.", function() {
+        	if (node_env === 'development'){
+            	expect(db_config[node_env].database).toBe('');
+        	} else if (node_env === 'test') {
+        		expect(db_config[node_env].database).toBe('');
+        	} else if (node_env === 'staging') {
+        		expect(db_config[node_env].database).toBe('');
+        	} else if (node_env === 'production') {
+        		expect(db_config[node_env].database).toBe('');
+        	}
         });
     });
 });
