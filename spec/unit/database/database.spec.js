2beadc79443b5da6ff3b60e3e8b6d0d33d984513
andela-eakinyele <emmanuel.akinyele@andela.com>
2015-11-09 14:41:31 +0100

    refactor(core): env variable in test .spec file

--- spec/unit/database/database.spec.js
+++ spec/unit/database/database.spec.js
@@ -1,12 +1,11 @@
 var path = require("path");
 var relativePath = path.relative("spec/unit/database", "src/config/config");
 var dbConfig = require(relativePath);
+var databaseName;
 describe("Database", function() {
   describe("Application", function() {
-    var databaseURI, databaseName;
     beforeEach(function() {
-      databaseURI = process.env.KIPIT_DATABASE_URI;
-      databaseName = path.basename(databaseURI);
+      databaseName = path.basename(process.env.KIPIT_TEST_DATABASE_URI);
     });
     it("should be connected to the correct database.", function() {
       expect(dbConfig.db.database).toEqual(databaseName);
