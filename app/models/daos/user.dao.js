e1802ab481d3ccc0e0c75ce2ecbfd2f157c5ec1f
rowland <rowlandekemezie@gmail.com>
2015-11-18 16:39:36 +0100

    feat(core): add user dao factory
    -add table and column parameters to user.dao.js

--- app/models/daos/user.dao.js
+++ app/models/daos/user.dao.js
@@ -3,6 +3,6 @@ var DA0 = require('./dao-factory');
 var db = require('../models/config/database');
 var userParams = require('../schema/user.json');
 
-db.connect();
+var client = db();
 
 var UserDAO = DAO("UserDAO", client, userParams);
