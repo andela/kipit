c198fdb80e5175e5a9959e04efd171e201879896
rowland <rowlandekemezie@gmail.com>
2015-11-30 10:00:02 +0100

    feat(routes): add routes to the api
    -implement route for users controller

--- app/config/express.js
+++ app/config/express.js
@@ -1,12 +1,20 @@
 // require modules
 var express = require("express");
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var db = require("./database");
 var app = express();
+var router = express.Router();
+
+var routes = require('../routes');
+routes(router);
 
 var client = db();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get("/",function(req,res){
-  res.json("Send me");
-});
+app.use('/api', router);
 
 module.exports = app;
